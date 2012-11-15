dd3886887b540c2531742c474a33ccf29e04f59b
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-11-15 14:53:50 +0100

    CassandraDataStore now supports batched insertions by calling write_row per row. Also cleaned up the code somewhat.

--- cassandralib/models.py
+++ cassandralib/models.py
@@ -13,47 +13,36 @@ COLNAME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 COLNAME_SEPERATOR = '_'
 
 
-class BucketSize:
-    HOURLY = 4
-    DAILY = 3
-    MONTHLY = 2
-    YEARLY = 1
+class BucketFormat:
+    HOURLY = '%Y-%m-%dT%H'
+    DAILY = '%Y-%m-%d'
+    MONTHLY = '%Y-%m'
+    YEARLY = '%Y'
 
 
-def bucket_size(sensor_id):
-    return BucketSize.YEARLY
+def bucket_format(sensor_id):
+    return BucketFormat.YEARLY
 
 
-def bucket_format(bucketsize):
-    if (bucketsize == BucketSize.HOURLY):
-        return '%Y-%m-%dT%H'
-    if (bucketsize == BucketSize.DAILY):
-        return '%Y-%m-%d'
-    if (bucketsize == BucketSize.MONTHLY):
-        return '%Y-%m'
-    if (bucketsize == BucketSize.YEARLY):
-        return '%Y'
-
-
-def bucket_delta(bucketsize):
-    if (bucketsize == BucketSize.HOURLY):
+def bucket_delta(bucketformat):
+    if (bucketformat == BucketFormat.HOURLY):
         return relativedelta( hours = +1 )
-    if (bucketsize == BucketSize.DAILY):
+    if (bucketformat == BucketFormat.DAILY):
         return relativedelta( days = +1 )
-    if (bucketsize == BucketSize.MONTHLY):
+    if (bucketformat == BucketFormat.MONTHLY):
         return relativedelta( months = +1 )
-    if (bucketsize == BucketSize.YEARLY):
+    if (bucketformat == BucketFormat.YEARLY):
         return relativedelta( years = +1 )
 
 
-def bucket_start(timestamp, bucketsize):
-    if (bucketsize == BucketSize.HOURLY):
+def bucket_start(timestamp, bucketformat):
+    if (bucketformat == BucketFormat.HOURLY):
         return timestamp.replace(minute=0, second=0, microsecond=0)
-    if (bucketsize == BucketSize.DAILY):
+    if (bucketformat == BucketFormat.DAILY):
         return timestamp.replace(hour=0, minute=0, second=0, microsecond=0)
-    if (bucketsize == BucketSize.MONTHLY):
+    if (bucketformat == BucketFormat.MONTHLY):
         return timestamp.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-    if (bucketsize == BucketSize.YEARLY):
+    if (bucketformat == BucketFormat.YEARLY):
         return timestamp.replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
 
 
@@ -63,7 +52,7 @@ class CassandraDataStore:
         pool = pycassa.ConnectionPool(keyspace=keyspace, server_list=nodes,
             prefill=False)
         self.cf = pycassa.ColumnFamily(pool, column_family)
-        self.qs = queue_size
+        self.batch = self.cf.batch(queue_size=queue_size)
 
     def read(self, sensor_id, start, end, params=[]):
         assert start.tzinfo != None, "Start datetime must be timezone aware"
@@ -75,12 +64,12 @@ class CassandraDataStore:
             str(end.tzinfo.utcoffset(end))[1:] == ':30:00', \
             "End datetime has weird utc offset; use tz.localize"
         
-        # The bucket size defines how much data is on one Cassandra row.
-        bucket = bucket_size(sensor_id)
+        # The bucket format defines how much data is on one Cassandra row.
+        format = bucket_format(sensor_id)
 
-        key_format = sensor_id + ":" + bucket_format(bucket)
-        stamp = bucket_start(start.astimezone(INTERNAL_TIMEZONE), bucket)
-        delta = bucket_delta(bucket)
+        key_format = sensor_id + ":" + format
+        stamp = bucket_start(start.astimezone(INTERNAL_TIMEZONE), format)
+        delta = bucket_delta(format)
 
         col_start = start.astimezone(INTERNAL_TIMEZONE).strftime(COLNAME_FORMAT)
         col_end = end.astimezone(INTERNAL_TIMEZONE).strftime(COLNAME_FORMAT)
@@ -90,13 +79,17 @@ class CassandraDataStore:
 
         # From each bucket within in the specified range, get the columns
         # within the specified range.
+        rowkeys = []
         while stamp < end:
-            print "Get " + stamp.strftime(key_format) + " with filter " + \
-                col_start + " " + col_end
-            try:
-                chunk = self.cf.get(stamp.strftime(key_format),
-                    column_start=col_start, column_finish=col_end)
-                for col_name in chunk:
+            rowkeys.append(stamp.strftime(key_format))
+            stamp += delta
+        print "Get " + repr(rowkeys) + " with filter " + \
+            col_start + " " + col_end
+        try:
+            result = self.cf.multiget(rowkeys,
+                column_start=col_start, column_finish=col_end)
+            for rowkey in result:
+                for col_name in result[rowkey]:
                     bits = col_name.split(COLNAME_SEPERATOR)
                     if (len(bits) > 1):
                         dt = datetime.strptime(bits[0], COLNAME_FORMAT)
@@ -104,12 +97,13 @@ class CassandraDataStore:
                         if (key in params) or (len(params) == 0):
                             if not dt in datetimes.keys():
                                 datetimes[dt] = {}
-                            datetimes[dt][key] = chunk[col_name]
+                            datetimes[dt][key] = result[rowkey][col_name]
                             if not key in data.keys():
                                 data[key] = []
-            except NotFoundException:
-                pass
-            stamp += delta
+        except NotFoundException:
+            pass
+        except Exception as ex:
+            print repr(ex)
         # Now, reform the data by series.
         for dt in sorted(datetimes):
             for key in data:
@@ -120,15 +114,18 @@ class CassandraDataStore:
         # And create the Pandas DataFrame.
         return pd.DataFrame(data=data, index=sorted(datetimes))
 
-    def write(self, sensor_id, df):
-        bucket = bucket_size(sensor_id)
-        key_format = bucket_format(bucket)
-        with self.cf.batch(queue_size=self.qs) as b:
-            for timestamp, row in df.iterrows():
-                ts_int = timestamp.astimezone(INTERNAL_TIMEZONE)
-                b.insert(
-                    ts_int.strftime(sensor_id + ':' + key_format),
-                    dict(("%s%s%s" % (ts_int.strftime(COLNAME_FORMAT),
-                        COLNAME_SEPERATOR, k), str(v))
-                        for k, v in row.to_dict().iteritems())
-                )
+    def write_frame(self, sensor_id, df):
+        for timestamp, row in df.iterrows():
+            self.write_row(sensor_id, timestamp, row.to_dict())
+
+    def write_row(self, sensor_id, timestamp, row):
+        ts_int = timestamp.astimezone(INTERNAL_TIMEZONE)
+        key = ts_int.strftime(sensor_id + ':' + bucket_format(sensor_id))
+        stamp = ts_int.strftime(COLNAME_FORMAT)
+        self.batch.insert(key, dict(
+            ("%s%s%s" % (stamp, COLNAME_SEPERATOR, k), str(v))
+            for k, v in row.iteritems()
+        ))
+
+    def flush(self):
+        self.batch.send()
